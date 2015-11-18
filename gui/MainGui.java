f6fedf99c19f4a32e25bdffcc6af1121ca882e6c
nathaniel <nathanielnaruto@163.com>
2015-11-19 00:57:01 +0800

    new interface

--- gui/MainGui.java
+++ gui/MainGui.java
@@ -1,32 +1,40 @@
 package gui;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.Component;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*; 
-import java.awt.FlowLayout;
 
 public class MainGui extends JFrame {
 	public MainGui() {
-		super("Library Management System");
+		super("图书管理系统");
 		try {
 			setSize(400, 300);
 			setLocationRelativeTo(null);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
-
-			JButton loginbut = new JButton("login");
-			JButton borrowbut = new JButton("borrow");
-			JButton returnbut = new JButton("return");
+			setLayout(new BorderLayout());
 			
-			loginbut.addActionListener(new LoginListener());
-			borrowbut.addActionListener(new BorrowListener());
-			returnbut.addActionListener(new ReturnListener());
+			String[] allType = {"书号","类别", "书名", "出版社", "年份", "作者"};
+			JComboBox chooseType = new JComboBox(allType);
 
-			add(borrowbut);
-			add(loginbut);
-			add(returnbut);
+			
+			JPanel searchPanel = new JPanel();
+			searchPanel.add(chooseType);
+			searchPanel.add(new JTextField(20));
+			searchPanel.add(new JButton("搜索"));
+
+			JButton loginBut = new JButton("管理员登陆");
+			JButton borrowBut = new JButton("借还");
+			
+			loginBut.addActionListener(new LoginListener());
+			borrowBut.addActionListener(new BorrowListener());
+			
+			JPanel optionPanel = new JPanel(new BorderLayout());
+			optionPanel.add(loginBut, BorderLayout.EAST);
+			optionPanel.add(borrowBut, BorderLayout.WEST);
+
+			add(searchPanel, BorderLayout.CENTER);
+			add(optionPanel, BorderLayout.SOUTH);
+
 			setVisible(true);
 		}
 		catch (Exception ex) {
@@ -55,15 +63,5 @@ public class MainGui extends JFrame {
 				});
 		}
 	}
-	class ReturnListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			setVisible(false);
-			ReturnGui returnwnd = new ReturnGui();
-			returnwnd.setAutoRecover(new WindowAdapter() {
-					public void windowClosing(WindowEvent e) {
-						setVisible(true);		
-					}
-				});
-		}
-	}
+	
 }
